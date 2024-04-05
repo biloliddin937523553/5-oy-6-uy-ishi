dcc4a387393d8c7a604b6866e5c94b7b65bfb278
biloliddin937523553 <biloliddinlatipov0@gmail.com>
2024-04-05 21:11:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let h1 = document.getElementById('h2')
+document.body.append(h1)  
+let color = prompt('textni rangi')
+let bgcolor = prompt('body rangi')
+h1.style.color = color
+document.body.style.background = bgcolor
